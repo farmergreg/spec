ea56a0ad6fa1c19960f26b7464e8448abea64c67
Gregory L. Dietsche <gregory.dietsche@cuw.edu>
2025-08-30 10:33:05 -0500

    refactor: clarify

--- src/pkg/adifield/adifield.go
+++ src/pkg/adifield/adifield.go
@@ -18,7 +18,7 @@ const (
 )
 
 // ADIField is the ADIF field name in and ADI file.
-// By convention in this package, field name constants are always UPPERCASE.
+// By convention in the adifield package, field name constants are always UPPERCASE.
 // This is a departure from the ADIF specification, which allows field names to include lowercase letters.
 type ADIField string
 
