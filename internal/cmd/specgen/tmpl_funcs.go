fc842d655ae558e45e16336947d4e789b9f5ed2f
Gregory L. Dietsche <gregory.dietsche@cuw.edu>
2025-09-03 20:39:15 -0500

    Revert "re-arrange and document"
    This reverts commit bee727e2c50c9fe654506e1a7a20d8f1d624cfa5.

--- internal/cmd/specgen/tmpl_funcs.go
+++ internal/cmd/specgen/tmpl_funcs.go
@@ -7,27 +7,14 @@ import (
 )
 
 var tmplFuncs = template.FuncMap{
-	"CommentsFor":    commentsFor,
 	"Split":          strings.Split,
-	"ToGoCode":       toGoCode,
 	"ToGoIdentifier": toGoIdentifier,
+	"ToGoCode":       toGoCode,
+	"CommentsFor": func(viewBag ViewBag, a any) string {
+		return viewBag.Comments(a)
+	},
 }
 
-// commentsFor retrieves comments for a given specification using the provided ViewBag.
-func commentsFor(viewBag ViewBag, a any) string {
-	return viewBag.Comments(a)
-}
-
-// toGoCode converts a struct to its Go code representation.
-// it removes the package prefix from the output because the code will be used in the same package.
-func toGoCode(a any, packageName string) string {
-	packageName += ".Spec"
-	code := fmt.Sprintf("%#v", a)
-	r := code[len(packageName):]
-	return r
-}
-
-// toGoIdentifier converts a string to a valid Go identifier by replacing or removing invalid characters.
 func toGoIdentifier(s string) string {
 	var replacer = strings.NewReplacer(
 		" ", "_",
@@ -46,4 +33,11 @@ func toGoIdentifier(s string) string {
 	result = strings.ReplaceAll(result, "__", "_")
 	result = strings.TrimSuffix(result, "_")
 	return result
+}
+
+func toGoCode(a any, packageName string) string {
+	packageName += ".Spec"
+	code := fmt.Sprintf("%#v", a)
+	r := code[len(packageName):]
+	return r
 }
