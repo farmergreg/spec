1e607151ac3160b9cc62082f096f8d9aa7ad2c1d
Gregory L. Dietsche <gregory.dietsche@cuw.edu>
2025-09-06 18:01:28 -0500

    add unit tests to verify generated code

---
+++ enum/antpath/antpath_test.go
@@ -0,0 +1,61 @@
+package antpath
+
+import (
+	"testing"
+)
+
+func TestLookup_Success(t *testing.T) {
+	tests := []struct {
+		name string
+		arg  AntPath
+		want Spec
+	}{
+		{name: "G", arg: G, want: internalMap[G]},
+		{name: "L", arg: L, want: internalMap[L]},
+		{name: "O", arg: O, want: internalMap[O]},
+		{name: "S", arg: S, want: internalMap[S]},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, ok := Lookup(tt.arg)
+			if !ok {
+				t.Errorf("Lookup(%v) = %v, want %v", tt.arg, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestLookup_Failure(t *testing.T) {
+	got, ok := Lookup("Z")
+	if ok {
+		t.Errorf("Lookup(Z) = %v, want nil", got)
+	}
+}
+
+func TestLookupByFilter_Success(t *testing.T) {
+	got := LookupByFilter(func(spec Spec) bool { return spec.Key == "G" })
+	if len(got) != 1 || got[0].Key != "G" {
+		t.Errorf("LookupByFilter(%v) = %v, want %v", "G", got, 1)
+	}
+}
+
+func TestLookupByFilter_Failure(t *testing.T) {
+	got := LookupByFilter(func(spec Spec) bool { return spec.Key == "A" })
+	if len(got) != 0 {
+		t.Errorf("LookupByFilter(%v) = %v, want %v", "A", got, 0)
+	}
+}
+
+func TestListActive(t *testing.T) {
+	got := ListActive()
+	if len(got) != 4 {
+		t.Errorf("ListActive() = %v, want %v", got, 4)
+	}
+}
+
+func TestList(t *testing.T) {
+	got := List()
+	if len(got) != 4 {
+		t.Errorf("List() = %v, want %v", got, 4)
+	}
+}
